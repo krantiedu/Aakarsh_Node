3003758ad17d5ff3e74c6177f03c3d90ceb9027a
Aakarshsahu <aakarshsshu2@gmail.com>
2023-06-22 00:40:24 +0530

    trail

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const mongoose = require("mongoose");
 const plm = require("passport-local-mongoose");
+
+
 // mongoose.connect("mongodb://127.0.0.1:27017/nextnode").then(function(){
 //   console.log("database successfully connected")
 // })
